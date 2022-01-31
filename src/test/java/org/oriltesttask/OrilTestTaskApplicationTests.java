52c37b4d0810de28b70e896a03ef5c9f953d3dce
PAKETA-0629 <kyrylo0629@gmail.com>
2022-01-31 10:12:49 +0200

    initial commit

---
+++ src/test/java/org/oriltesttask/OrilTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oriltesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrilTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
