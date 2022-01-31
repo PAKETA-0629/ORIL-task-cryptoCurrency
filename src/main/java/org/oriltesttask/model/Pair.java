c7639ec5dafaae060cace9bd3fffbf719a54d24f
PAKETA-0629 <kyrylo0629@gmail.com>
2022-01-31 20:55:31 +0200

    fix pair model

--- src/main/java/org/oriltesttask/model/Pair.java
+++ src/main/java/org/oriltesttask/model/Pair.java
@@ -1,6 +1,7 @@
 package org.oriltesttask.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,8 +9,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
+import java.time.OffsetDateTime;
 
+@Builder
 @Data
 @Entity
 @Table(name = "pairs_prices")
@@ -26,5 +28,9 @@ public class Pair {
     @Column(name = "price")
     private Double price;
     @Column(name = "date")
-    private Date date;
+    private OffsetDateTime date;
+
+    public PairProfile toProfile() {
+        return PairProfile.builder().price(price).symbol1(symbol1).symbol2(symbol2).date(date).build();
+    }
 }
