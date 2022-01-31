611e1a593f69c2f693b7782157329826dd3b92b9
PAKETA-0629 <kyrylo0629@gmail.com>
2022-01-31 11:42:54 +0200

    add pair repository

---
+++ src/main/java/org/oriltesttask/repository/PairRepository.java
@@ -0,0 +1,30 @@
+package org.oriltesttask.repository;
+
+import org.oriltesttask.model.Pair;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PairRepository extends JpaRepository<Pair, Long> {
+
+    @Override
+    Optional<Pair> findById(Long id);
+
+    @Override
+    List<Pair> findAll();
+
+    @Query(nativeQuery = true, value = "SELECT * FROM pairs_prices WHERE price = (SELECT MIN(price) FROM pairs_prices WHERE symbol1=:currency_name);")
+    Optional<Pair> findMinPrice(@Param(value = "currency_name")String currencyName);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM pairs_prices WHERE price = (SELECT MAX(price) FROM pairs_prices WHERE symbol1=:currency_name);")
+    Optional<Pair> findMaxPrice(@Param(value = "currency_name")String currencyName);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "INSERT INTO pairs_prices(symbol1, symbol2, price) VALUES(:symbol1, :symbol2, :price)")
+    void createPair(@Param(value = "symbol1") String symbol1, @Param(value = "symbol2") String symbol2, @Param(value = "price") Double price);
+}
