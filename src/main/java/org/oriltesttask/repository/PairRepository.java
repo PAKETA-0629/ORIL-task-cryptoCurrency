d49b80156f430a5149ed129e07a2d4e9405a3406
PAKETA-0629 <kyrylo0629@gmail.com>
2022-01-31 20:54:41 +0200

    fix create quary

--- src/main/java/org/oriltesttask/repository/PairRepository.java
+++ src/main/java/org/oriltesttask/repository/PairRepository.java
@@ -3,12 +3,13 @@ package org.oriltesttask.repository;
 import org.oriltesttask.model.Pair;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,9 +28,10 @@ public interface PairRepository extends JpaRepository<Pair, Long> {
     @Query(nativeQuery = true, value = "SELECT * FROM pairs_prices WHERE price = (SELECT MAX(price) FROM pairs_prices WHERE symbol1=:currency_name);")
     Optional<Pair> findMaxPrice(@Param(value = "currency_name")String currencyName);
 
+    @Transactional
     @Modifying
-    @Query(nativeQuery = true, value = "INSERT INTO pairs_prices(symbol1, symbol2, price) VALUES(:symbol1, :symbol2, :price)")
-    void createPair(@Param(value = "symbol1") String symbol1, @Param(value = "symbol2") String symbol2, @Param(value = "price") Double price);
+    @Query(nativeQuery = true, value = "INSERT INTO pairs_prices(symbol1, symbol2, price, date) VALUES(:symbol1, :symbol2, :price, :date)")
+    void createPair(@Param(value = "symbol1") String symbol1, @Param(value = "symbol2") String symbol2, @Param(value = "price") Double price, @Param(value = "date") OffsetDateTime date);
 
     @Query(nativeQuery = true, value = "SELECT * FROM pairs_prices WHERE symbol1 = :currency_name")
     Page<Pair> getPage(@Param(value = "currency_name")String currencyName, Pageable pageable);
